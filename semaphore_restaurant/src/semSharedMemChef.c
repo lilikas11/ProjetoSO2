de3cb7111f623ee3e751a620e86d97329471896e
lilikas11 <lilianapcribeiro@gmail.com>
2022-12-31 02:06:39 +0000

    from chef function waitForOrder looks ready

--- semaphore_restaurant/src/semSharedMemChef.c
+++ semaphore_restaurant/src/semSharedMemChef.c
@@ -31,7 +31,6 @@
 #include "semaphore.h"
 #include "sharedMemory.h"
 
-
 /** \brief logging file name */
 static char nFic[51];
 
@@ -44,54 +43,60 @@ static int semgid;
 /** \brief pointer to shared memory region */
 static SHARED_DATA *sh;
 
-static void waitForOrder ();
-static void processOrder ();
+static void waitForOrder();
+static void processOrder();
 
 /**
  *  \brief Main program.
  *
  *  Its role is to generate the life cycle of one of intervening entities in the problem: the chef.
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int key;                                          /*access key to shared memory and semaphore set */
-    char *tinp;                                                     /* numerical parameters test flag */
+    int key;    /*access key to shared memory and semaphore set */
+    char *tinp; /* numerical parameters test flag */
 
     /* validation of command line parameters */
 
-    if (argc != 4) { 
-        freopen ("error_PT", "a", stderr);
-        fprintf (stderr, "Number of parameters is incorrect!\n");
+    if (argc != 4)
+    {
+        freopen("error_PT", "a", stderr);
+        fprintf(stderr, "Number of parameters is incorrect!\n");
         return EXIT_FAILURE;
     }
-    else {
-       freopen (argv[3], "w", stderr);
-       setbuf(stderr,NULL);
+    else
+    {
+        freopen(argv[3], "w", stderr);
+        setbuf(stderr, NULL);
     }
-    strcpy (nFic, argv[1]);
-    key = (unsigned int) strtol (argv[2], &tinp, 0);
-    if (*tinp != '\0') {
-        fprintf (stderr, "Error on the access key communication!\n");
+    strcpy(nFic, argv[1]);
+    key = (unsigned int)strtol(argv[2], &tinp, 0);
+    if (*tinp != '\0')
+    {
+        fprintf(stderr, "Error on the access key communication!\n");
         return EXIT_FAILURE;
     }
 
     /* connection to the semaphore set and the shared memory region and mapping the shared region onto the
        process address space */
-    if ((semgid = semConnect (key)) == -1) { 
-        perror ("error on connecting to the semaphore set");
+    if ((semgid = semConnect(key)) == -1)
+    {
+        perror("error on connecting to the semaphore set");
         return EXIT_FAILURE;
     }
-    if ((shmid = shmemConnect (key)) == -1) { 
-        perror ("error on connecting to the shared memory region");
+    if ((shmid = shmemConnect(key)) == -1)
+    {
+        perror("error on connecting to the shared memory region");
         return EXIT_FAILURE;
     }
-    if (shmemAttach (shmid, (void **) &sh) == -1) { 
-        perror ("error on mapping the shared region on the process address space");
+    if (shmemAttach(shmid, (void **)&sh) == -1)
+    {
+        perror("error on mapping the shared region on the process address space");
         return EXIT_FAILURE;
     }
 
     /* initialize random generator */
-    srandom ((unsigned int) getpid ());                                      
+    srandom((unsigned int)getpid());
 
     /* simulation of the life cycle of the chef */
 
@@ -100,9 +105,11 @@ int main (int argc, char *argv[])
 
     /* unmapping the shared region off the process address space */
 
-    if (shmemDettach (sh) == -1) { 
-        perror ("error on unmapping the shared region off the process address space");
-        return EXIT_FAILURE;;
+    if (shmemDettach(sh) == -1)
+    {
+        perror("error on unmapping the shared region off the process address space");
+        return EXIT_FAILURE;
+        ;
     }
 
     return EXIT_SUCCESS;
@@ -113,45 +120,58 @@ int main (int argc, char *argv[])
  *
  *  The chef waits for the food request that will be provided by the waiter.
  */
-static void waitForOrder ()
+static void waitForOrder()
 {
     /* insert your code here */
-
-    if (semDown (semgid, sh->mutex) == -1) {                                                      /* enter critical region */
-        perror ("error on the up operation for semaphore access (PT)");
+    if (semDown(semgid, sh->waitOrder) == -1) {                                                     
+        perror ("error on the down operation for waitOrder semaphore access (CH)");
         exit (EXIT_FAILURE);
+    }
+    /* end my code */
+
+    if (semDown(semgid, sh->mutex) == -1)
+    { /* enter critical region */
+        perror("error on the up operation for semaphore access (PT)");
+        exit(EXIT_FAILURE);
     }
 
     /* insert your code here */
+    sh->fSt.st.chefStat = COOK;
+    sh->fSt.foodOrder = 0;
+    saveState(nFic, &sh->fSt);
+    /* end my code */
 
-    if (semUp (semgid, sh->mutex) == -1) {                                                      /* exit critical region */
-        perror ("error on the up operation for semaphore access (PT)");
-        exit (EXIT_FAILURE);
+
+    if (semUp(semgid, sh->mutex) == -1)
+    { /* exit critical region */
+        perror("error on the up operation for semaphore access (PT)");
+        exit(EXIT_FAILURE);
     }
 }
 
 /**
- *  \brief chef cooks, then delivers the food to the waiter and then rests 
+ *  \brief chef cooks, then delivers the food to the waiter and then rests
  *
  *  The chef updates its state and signals the waiter that food is ready
  *  The internal state should be saved.
  */
-static void processOrder ()
+static void processOrder()
 {
-    usleep((unsigned int) floor ((MAXCOOK * random ()) / RAND_MAX + 100.0));
+    usleep((unsigned int)floor((MAXCOOK * random()) / RAND_MAX + 100.0));
 
-    if (semDown (semgid, sh->mutex) == -1) {                                                      /* enter critical region */
-        perror ("error on the up operation for semaphore access (PT)");
-        exit (EXIT_FAILURE);
+    if (semDown(semgid, sh->mutex) == -1)
+    { /* enter critical region */
+        perror("error on the up operation for semaphore access (PT)");
+        exit(EXIT_FAILURE);
     }
 
     /* insert your code here */
 
-    if (semUp (semgid, sh->mutex) == -1) {                                                      /* exit critical region */
-        perror ("error on the up operation for semaphore access (PT)");
-        exit (EXIT_FAILURE);
+    if (semUp(semgid, sh->mutex) == -1)
+    { /* exit critical region */
+        perror("error on the up operation for semaphore access (PT)");
+        exit(EXIT_FAILURE);
     }
 
     /* insert your code here */
 }
-
